687c756cb4d2d423cfeeec60d62c4440ff111038
xiaochengyez <48740941+xiaochengyez@users.noreply.github.com>
2019-09-05 17:50:36 +0800

    1

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
+from news import models
+
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ['username','email','signature']
+    search_fields = ('username','email')
+    list_filter = ('email',)
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title','pub_date','account')
+    search_fields = ('title',)
+    list_filter = ('account','pub_date')
+    #fields = ('title','content')
+
+admin.site.register(models.Account,AccountAdmin)
+admin.site.register(models.Article,ArticleAdmin)
+admin.site.register(models.Tag)
--- news/models.py
+++ news/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
 # Create your models here.
+class Account(models.Model):
+    """账户表"""
+    username = models.CharField(max_length=64,unique=True)
+    email = models.CharField(max_length=100,unique=True)
+    password = models.CharField(max_length=255)
+    register_data = models.DateTimeField(auto_now_add=True)
+    signature = models.CharField("签名",max_length=255,null=True,blank=True)
+    class Meta:
+        verbose_name_plural = '账户'
+    def __str__(self):
+        return self.username
+
+class Article(models.Model):
+    title = models.CharField(max_length=255,unique=True)
+    content = models.TextField()
+    account = models.ForeignKey("Account",on_delete=models.CASCADE)
+    tags = models.ManyToManyField("Tag",null=True)
+    pub_date = models.DateField(auto_now_add=True)
+    class Meta:
+        verbose_name_plural = '文章'
+
+    def get_comment(self):
+        return 10
+
+    def get_tages(self):
+        return ','.join([i.name for i in self.tags.all()])
+
+    def __str__(self):
+        return self.title
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=64,unique=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
